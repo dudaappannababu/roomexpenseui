4355eadda89ff892afa15e61bd158773d868cf0c
apduda <apduda@deloitte.com>
2024-03-23 15:53:16 +0530

    Remove unnecessary content and add deployment link in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import Dashboard from './Pages/Dashboard';
 
+// To deploy in github pages refer this link "https://www.youtube.com/watch?v=Q9n2mLqXFpU"
+
 function App() {
   return (
     <div className="App">
